6f1cb3414f130ac5783c6d324ed260a76666c642
gsaha <gourab.saha@cern.ch>
2026-09-08 15:59:08 +0200

    emu main analysis, calibration, selection, triggers, categorization, production from Cyril

--- zttpol/calibration/calibrate_emu.py
+++ zttpol/calibration/calibrate_emu.py
@@ -10,15 +10,19 @@ import law
 import functools
 
 from columnflow.calibration import Calibrator, calibrator
-from columnflow.calibration.cms.jets import jets
-from columnflow.production.cms.seeds import deterministic_seeds
-from columnflow.util import maybe_import
+from columnflow.calibration.cms.muon import muon_sr
+from columnflow.production.cms.electron import electron_sceta
+from columnflow.calibration.cms.egamma import electron_scale_smear
+from columnflow.calibration.cms.tau import tec
+
 from columnflow.columnar_util import set_ak_column
 from columnflow.columnar_util import optional_column as optional
+from columnflow.columnar_util import IF_DATA, IF_MC
+
 from columnflow.production.util import attach_coffea_behavior
 
-from zttpol.calibration.electron import electron_smearing_scaling
-from zttpol.calibration.tau import tau_energy_scale
+from columnflow.util import maybe_import
+
 from zttpol.calibration.calibrate_base import calibrate_base
 
 from zttpol.util import IF_RUN2, IF_RUN3
@@ -38,11 +42,11 @@ logger = law.logger.get_logger(__name__)
 @calibrator(
     uses={
         calibrate_base,
-        electron_smearing_scaling,
+        electron_sceta,
     },
     produces={
         calibrate_base,
-        electron_smearing_scaling,
+        electron_sceta,
     },
     exposed=True,
 )
@@ -53,10 +57,88 @@ def calibrate_emu(self: Calibrator, events: ak.Array, **kwargs) -> ak.Array:
         - Electron Scale & Smearing Correction (Smearing: MC only, Scale: DATA only)
     """
 
+    task = kwargs['task']
     # base calibrator
-    events = events[calibrate_base](events)
-    
-    # electron scale and smearing correction
-    events = self[electron_smearing_scaling](events)
-        
+    events = self[calibrate_base](events, task=task)
+
+    # optional electron sceta production
+    if "superclusterEta" not in events.Electron.fields:
+        events = self[electron_sceta](events, **kwargs)
+
+    # data/mc specific calibrations
+    if self.dataset_inst.is_data:
+        # nominal ess
+        events = self[self.ess_nominal_cls](events, **kwargs)
+        if self.has_dep(self.muon_sr_nominal_cls):
+            # nominal muon scale and resolution
+            events = self[self.muon_sr_nominal_cls](events, **kwargs)
+    else:
+        if self.has_dep(self.muon_sr_full_cls):
+            # full muon scale and resolution
+            events = self[self.muon_sr_full_cls](events, **kwargs)
+        # full ess
+        events = self[self.ess_full_cls](events, **kwargs)
+        # full tec
+        events = self[self.tec_nominal_cls](events, **kwargs)
+                
     return events
+
+
+@calibrate_emu.init
+def calibrate_emu_init(self: Calibrator, **kwargs) -> None:
+    super(calibrate_emu, self).init_func(**kwargs)
+
+    # set the name of the met collection to use
+    met_name = self.config_inst.x.met_name
+    raw_met_name = self.config_inst.x.raw_met_name
+
+    # derive calibrators to add settings once
+    flag = f"custom_calibs_registered_{self.cls_name}"
+    if not self.config_inst.x(flag, False):
+        def add_calib_cls(name, base, cls_dict=None):
+            self.config_inst.set_aux(f"calib_{name}_cls", base.derive(name, cls_dict=cls_dict or {}))
+
+
+        # derive tec calibrators
+        add_calib_cls("tec_nominal", tec, cls_dict={
+            "met_name": met_name,
+            "propagate_met": False,  # not needed after JET-to-MET propagation
+            "with_uncertainties": False,
+        })
+        # derive electron scale and resolution calibrators
+        add_calib_cls("ess_full", electron_scale_smear)
+        add_calib_cls("ess_nominal", electron_scale_smear, cls_dict={
+            "with_uncertainties": False,
+        })
+
+        # derive muon scale and resolution calibrators
+        add_calib_cls("muon_sr_full", muon_sr, cls_dict={
+            "store_original": True,
+        })
+        add_calib_cls("muon_sr_nominal", muon_sr, cls_dict={
+            "store_original": True,
+            "with_uncertainties": False,
+        })
+
+        # change the flag
+        self.config_inst.set_aux(flag, True)
+
+
+    # store references to classes
+    self.tec_nominal_cls = self.config_inst.x.calib_tec_nominal_cls
+    self.ess_full_cls = self.config_inst.x.calib_ess_full_cls
+    self.ess_nominal_cls = self.config_inst.x.calib_ess_nominal_cls
+    self.muon_sr_full_cls = self.config_inst.x.calib_muon_sr_full_cls
+    self.muon_sr_nominal_cls = self.config_inst.x.calib_muon_sr_nominal_cls
+    
+    # collect derived calibrators and add them to the calibrator uses and produces
+    derived_calibrators = {
+        self.tec_nominal_cls,
+        self.ess_full_cls,
+        self.ess_nominal_cls,
+        ~IF_RUN2(self.muon_sr_full_cls),
+        ~IF_RUN2(self.muon_sr_nominal_cls),
+    }
+
+    self.uses |= derived_calibrators
+    self.produces |= derived_calibrators        
---
+++ zttpol/production/produce_emu.py
@@ -0,0 +1,97 @@
+# coding: utf-8
+
+"""
+Column production methods related to higher-level features.
+"""
+import functools
+
+import law
+import order as od
+from typing import Optional
+from columnflow.production import Producer, producer
+
+from columnflow.util import maybe_import
+
+from columnflow.columnar_util import EMPTY_FLOAT, Route, set_ak_column, remove_ak_column
+from columnflow.columnar_util import optional_column as optional
+
+from columnflow.config_util import get_events_from_categories
+from zttpol.production.ReArrangeZcandProds import reArrangeDecayProducts, reArrangeGenDecayProducts
+from zttpol.production.ProduceObservables import ProduceRecoObservables, ProduceGenObservables
+
+from zttpol.production.weights import (
+    muon_id_weights,
+    muon_iso_weights,
+    muon_trigger_weights,
+    electron_id_weights,
+    electron_reco_weights,
+)
+
+from zttpol.util import (
+    IF_DATASET_HAS_LHE_WEIGHTS,
+    IF_DATASET_IS_DY,
+    IF_DATASET_IS_W,
+    IF_DATASET_IS_SIGNAL,
+    IF_DATASET_IS_TT
+)
+from zttpol.util import (
+    IF_RUN2,
+    IF_RUN3,
+    IF_ALLOW_STITCHING,
+    IF_GENMATCH,
+    IF_GENMATCH_ON_FOR_SIGNAL,
+    transverse_mass
+)
+
+from zttpol.production.produce_base import produce_base
+
+
+np = maybe_import("numpy")
+ak = maybe_import("awkward")
+coffea = maybe_import("coffea")
+maybe_import("coffea.nanoevents.methods.nanoaod")
+
+# helpers
+set_ak_column_f32 = functools.partial(set_ak_column, value_type=np.float32)
+set_ak_column_i32 = functools.partial(set_ak_column, value_type=np.int32)
+
+logger = law.logger.get_logger(__name__)
+
+
+@producer(
+    uses={
+        produce_base,
+        # -- muon -- #
+        "Muon.{pt,eta,phi,mass}",
+        muon_id_weights,
+        muon_iso_weights,
+        # -- electron -- #
+        "Electron.{pt,eta,phi,mass}",
+        electron_id_weights,
+        electron_reco_weights,
+    },
+    produces={
+        produce_base,
+        # -- muon -- #
+        muon_id_weights,
+        muon_iso_weights,
+        # -- electron -- #
+        electron_id_weights,
+        electron_reco_weights,
+    },
+)
+def produce_emu(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
+
+    events = self[produce_base](events, **kwargs)
+
+    #from IPython import embed; embed()
+    
+    if self.dataset_inst.is_mc:
+        events = self[muon_id_weights](events, **kwargs)
+        events = self[muon_iso_weights](events, **kwargs)
+        events = self[electron_id_weights](events, **kwargs)
+        events = self[electron_reco_weights](events, **kwargs)
+
+    #events = self[ff_weight](events, **kwargs)        
+    
+    return events
